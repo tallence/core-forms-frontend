461fd0e1fb70df744fa59da7001d24fda31f27a0
sven.kroschwald <sven.kroschwald@tallence.com>
2022-08-25 09:23:13 +0200

    updated to Vue 2.7; added HiddenField

---
+++ src/vue/plugins/core-forms/fields/HiddenField.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="hidden"
+         :name="field.technicalName"
+         :value="field.value"
+         :id="field.id"
+         readonly/>
+</template>
+
+<script>
+  export default {
+    props: ['field']
+  }
+</script>
